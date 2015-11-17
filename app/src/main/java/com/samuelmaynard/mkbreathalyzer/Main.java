e2f1165340015b56b675a609208408bcdac6be5b
samm81 <samwmaynard@gmail.com>
2015-11-16 22:09:54 -0600

    basic functionality

--- app/src/main/java/com/samuelmaynard/mkbreathalyzer/Main.java
+++ app/src/main/java/com/samuelmaynard/mkbreathalyzer/Main.java
@@ -1,21 +1,39 @@
 package com.samuelmaynard.mkbreathalyzer;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RatingBar;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class Main extends AppCompatActivity {
 
     MediaPlayer daylight;
     boolean playing;
+
+    String[] bacs = {
+            "have you even had a drink?",
+            "you're good for another couple shots",
+            "you might want to start holding up there cowboy",
+            "put the bailey's down",
+            "dude I've been patting your head for an hour go to bed"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,39 +42,35 @@ public class Main extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         daylight = MediaPlayer.create(getApplicationContext(), R.raw.daylight);
         playing = false;
+
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Log.i("Main", "rating: " + rating);
+                String bac = bacs[(int)rating - 1];
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Main.this);
+                alertDialogBuilder.setMessage(bac)
+                        .setCancelable(true)
+                        .setPositiveButton("back", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = alertDialogBuilder.create();
+                alert.show();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void playpause(View view) {
